03583c7a2d5aa899d1cdab6338c3da25c216dcbc
Stefan Proell <info@stefanproell.at>
2015-01-07 15:38:02 +0100

    test

--- Parent/src/main/java/org/example/App.java
+++ Parent/src/main/java/org/example/App.java
@@ -1,13 +1,12 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ModuleB_App mod = new ModuleB_App();
+
     }
 }
