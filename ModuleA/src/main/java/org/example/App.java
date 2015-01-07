aeacd562277037fb5a00d072f6094d8f1d986c86
Stefan Proell <info@stefanproell.at>
2015-01-07 14:21:32 +0100

    Initial commit

---
+++ ModuleA/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import at.stefanproell.PersistentIdentifierMockup.Helpers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Helpers h = new Helpers();
+
+    }
+}
